c17021e32a2110ee7a5b67c5e87dd2396af6d240
Erkin Tekin <erkintekin12@gmail.com>
2025-01-05 04:16:29 +0300

    crud işlemleri tamamlandı

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -46,12 +46,46 @@ exports.getProductById = async (req, res) => {
   try {
     const product = await knex("products").where({ id }).first(); // Paramstaki IDye eşit olan ilk eşleşmeyi getir
     if (!product) {
-      res.status(404).json({ message: "Ürün bulunamadı" });
+      return res.status(404).json({ message: "Ürün bulunamadı" });
     }
     res.json(product);
   } catch (err) {
     res
       .status(500)
       .json({ message: "Internal servis hatası", error: err.message });
+  }
+};
+
+// Spesifik bir ürünü güncelleme
+exports.editProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, category, price, stock, description, image_url } = req.body;
+  try {
+    const updateRows = await knex("products")
+      .where({ id })
+      .update({ name, category, price, stock, description, image_url });
+    if (!updateRows) {
+      return res.status(404).json({ message: "Ürün bulunamadı" });
+    }
+    res.status(200).json({ message: "Ürün başarıyla güncellendi" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal servis hatası", error: err.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteRows = await knex("products").where({ id }).del();
+    if (!deleteRows) {
+      return res.status(404).json({ message: "Ürün bulunamadı" });
+    }
+    res.status(200).json({ message: "Ürün başarıyla silindi" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: err.message });
   }
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,14 +3,23 @@ const {
   createProduct,
   getProducts,
   getProductById,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/productController");
 const authenticateToken = require("../middleware/authMiddleware");
 const allowRoles = require("../middleware/roleMiddleware");
 const { get } = require("./userRoutes");
+const { update } = require("../config/knex");
 const router = express.Router();
 
-router.get("/", authenticateToken, getProducts);
+router.get("/add", authenticateToken, getProducts);
 router.get("/:id", authenticateToken, getProductById);
-router.post("/", authenticateToken, allowRoles(2), createProduct); // Admine create yetkisi verilmesi
-
+router.post("/list", authenticateToken, allowRoles(1, 2), createProduct); // SuperAdmin ve Admin'e create yetkisi verilmesi
+router.put("/edit/:id", authenticateToken, allowRoles(1, 2), updateProduct); // SuperAdmin ve Admin'e edit yetkisi verilmesi
+router.delete(
+  "/delete/:id",
+  authenticateToken,
+  allowRoles(1, 2),
+  deleteProduct
+); // SuperAdmin ve Admin'e delete yetkisi verilmesi
 module.exports = router;
