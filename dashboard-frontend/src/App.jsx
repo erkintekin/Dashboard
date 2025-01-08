6d7e88acf87f39c0bafffb3b5dbd1174740fa720
Erkin Tekin <erkintekin12@gmail.com>
2025-01-08 05:01:37 +0300

    settings eklendi

--- dashboard-frontend/src/App.jsx
+++ dashboard-frontend/src/App.jsx
@@ -4,8 +4,10 @@ import Sidebar from "./components/common/Sidebar";
 
 import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
+import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
@@ -20,8 +22,10 @@ function App() {
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/sales" element={<SalesPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
